71bcf5a45067be2263641f77e391acd2e8f4f629
SydMontague <sydmontague@phoenix-staffel.de>
2019-10-20 16:20:01 +0200

    Add project files.

---
+++ DSCSTools/DSCSTools.cpp
@@ -0,0 +1,12 @@
+﻿// DSCSTools.cpp : Defines the entry point for the application.
+//
+
+#include "DSCSTools.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
