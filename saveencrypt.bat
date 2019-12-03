04df8d467fcd1db1d0cdf5c20ddd63da9b7596c8
Marcus Carter <cartermarcus04@yahoo.com>
2019-12-02 20:57:51 -0500

    Update saveencrypt.bat

--- saveencrypt.bat
+++ saveencrypt.bat
@@ -1,5 +1,9 @@
 cd C:\DSCSTools\
 if exist 0000_dec.bin DSCSTools --saveencrypt 0000_dec.bin 0000.bin
+if exist 0001_dec.bin DSCSTools --saveencrypt 0001_dec.bin 0001.bin
+if exist 0002_dec.bin DSCSTools --saveencrypt 0002_dec.bin 0002.bin
 if exist slot_0000_dec.bin DSCSTools --saveencrypt slot_0000_dec.bin slot_0000.bin
+if exist slot_0001_dec.bin DSCSTools --saveencrypt slot_0001_dec.bin slot_0001.bin
+if exist slot_0002_dec.bin DSCSTools --saveencrypt slot_0002_dec.bin slot_0002.bin
 if exist system_graphic_dec.bin DSCSTools --saveencrypt system_graphic_dec.bin system_graphic.bin
 if exist system_data_dec.bin DSCSTools --saveencrypt system_data_dec.bin system_data.bin
